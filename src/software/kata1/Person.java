5f2f617df9f4592576f8c9854067900c81dc366f
Naixin <naixin.chen101@alu.ulpgc.es>
2023-09-28 21:06:08 +0100

    Main class created

--- src/software/kata1/Person.java
+++ src/software/kata1/Person.java
@@ -1,4 +1,30 @@
 package software.kata1;
 
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    @Override
+    public String toString() {
+        return String.format("%s is %d years old. Birthday: %s", this.name, this.getAge(), this.birthday);
+    }
+
+    private int getAge() {
+        return (int) ((LocalDate.now().toEpochDay() - birthday.toEpochDay()) / 365.25);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
